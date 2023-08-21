afd7f52b6bcb84b3b1d3c952ca47767ffeb651aa
Shyngys Sailau <shino_099@mail.ru>
2023-08-21 16:43:00 +0600

    final version without APIs

---
+++ app/src/main/java/com/example/cyberelectronics/ApiService.java
@@ -0,0 +1,14 @@
+package com.example.cyberelectronics;
+
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+
+public interface ApiService {
+//    @GET("endpoint") // Replace with your API endpoint
+//    @POST("endpoint") // Replace with your API endpoint
+//    @PUT("endpoint") // Replace with your API endpoint
+//    @DELETE("endpoint") // Replace with your API endpoint
+//    Call<ApiResponse> getApiResponse();
+}
---
+++ app/src/main/java/com/example/cyberelectronics/RetrofitInstance.java
@@ -0,0 +1,19 @@
+package com.example.cyberelectronics;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+//    private static Retrofit retrofit;
+//    private static final String BASE_URL = "http://localhost:8080/registration";
+//    public static Retrofit getRetrofitInstance(){
+//        if (retrofit == null){
+//            retrofit = new retrofit2.Retrofit.Builder()
+//            .baseUrl(BASE_URL) // Base URL of the API
+//            .addConverterFactory(GsonConverterFactory.create()) // JSON converter
+//            .build();
+//        }
+//        return retrofit;
+//    }
+
+}
